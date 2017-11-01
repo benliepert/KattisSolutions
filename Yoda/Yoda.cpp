105b1a59b334894745fae62d8355c3f7c66022fe
benliepert <ben.liepert@gmail.com>
2017-10-31 21:32:17 -0400

    adding what's done of Yoda

--- Yoda/Yoda.cpp
+++ Yoda/Yoda.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
+
+int max(int, int);
 
 
 int main()
 {
+    int iNum1, iNum2;
+    cin >> iNum1 >> iNum2;
 
+    string sNumOne = to_string(iNumOne);
+    string sNumTwo = to_string(iNumTwo);
+
+    int iLen1 = sNumOne.size();
+    int iLen2 = sNumTwo.size();
+
+    int iMaxLen = max(iLen1, iLen2);
+    
 
     return 0;
+}
+
+int main(int a, int b)
+{
+    return a > b ? a : b;
 }
